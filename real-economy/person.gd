69bafdea73e36317916ca12a2396a32c4c087db8
Churchill <churchillsteve05@gmail.com>
2026-09-01 00:21:09 +0100

    Add files via upload

--- real-economy/person.gd
+++ real-economy/person.gd
@@ -1,20 +1,26 @@
 extends CharacterBody3D
-
+class_name Person
 ##Tracks owned locations such as farm
 ##could later be put in a seperate portfolio
 ##node alongside [member purse]? if that would be easier
+@export
 var ownedLocations = []
 ##Tracks accumulated currencies - also see [member ownedLocations]
+@export
 var purse = {"dollars" : 100, "pounds" : 25}
 ##Faction this person belongs to.
-var faction
+@export
+var faction =load("res://resources/faction.gd")
 ##List of employees
+@export
 var employees = []
 ##Link to employer
-var employer 
+@export
+var employer : Person
+
 var speed = 2
 var target : Vector3 
-var localMesh
+##link to employmentType
 var employment = employmentType.new()
 func _ready() -> void:
 	target=self.global_position
--- real-economy/resources/faction.gd
+++ real-economy/resources/faction.gd
@@ -4,8 +4,9 @@ var factionName : String = ""
 ##list of all members of the faction
 var memberList = []
 ##A list of all employers in this faction
-var employersInFaction
-
+var employersInFaction = []
+##leader of the faction
+var leader
 ##The employer in question would trigger this to change
 ##to or from the employer list
 func updateEmployers(employerToUpdate):
