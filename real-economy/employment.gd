76bb4492a6459e4fb0c6f3d1080cca0b8ee6319c
Churchill <churchillsteve05@gmail.com>
2026-08-31 02:44:13 +0100

    Add files via upload

--- real-economy/employment.gd
+++ real-economy/employment.gd
@@ -1,4 +1,7 @@
 extends Node3D
 
 var employmentLocation
-enum payType {regular, nonRegular} 
+var employmentType
+func _ready() -> void:
+	var tempType = load("res://employment types/employment_farmer.tscn")
+	employmentType = tempType.instantiate()
