681a0bb9bee6764a6c297507d7fe3d5c1ccb82ae
Churchill <churchillsteve05@gmail.com>
2026-09-05 03:17:34 +0100

    Add files via upload

--- real-economy/farm.gd
+++ real-economy/farm.gd
@@ -9,6 +9,16 @@ var ownerOf : Person
 var ownerIsEmployer = false
 
 var slotsAvailable = 4
-var workersToiling : Array[Person]
+var workersToiling : Array[Person] = []
 
 var itemProduced : String = "Wheat"
+var workNeeded : int = 4000
+
+func addWorkerToThis(thisWorker):
+	if! workersToiling.has(thisWorker):
+		workersToiling.append(thisWorker)
+		$workingTime.timeout.connect(_on_working_time_timeout.bind(thisWorker))
+		$workingTime.start(thisWorker)
+
+func _on_working_time_timeout(thisWorker) :
+	workersToiling.get(thisWorker)
